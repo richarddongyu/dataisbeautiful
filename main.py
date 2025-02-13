f3a4be943905da0ad199539413cf32b7dc649b02
ruby.dongle <ruby.dongle@gmail.com>
2025-02-13 16:57:10 +0800

    init code

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import dtseek as dt
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    df = dt.get_tick_data('600868')
+
+    print(df)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
